28d90cbbf22691cc30a0cabdf2e4aa2b35512b22
Hmzask <hamzask2026@gmail.com>
2026-05-20 18:39:55 +0530

    initial config commits

---
+++ api-service/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+def create_app():
+
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return {
+            "message": "Enterprise AI Platform"
+        }
+
+    return app
