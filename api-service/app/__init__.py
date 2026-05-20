0aeee8c93d57c040ea8d479a38c7ac75eea1c0ab
Hmzask <hamzask2026@gmail.com>
2026-05-20 19:01:52 +0530

    initial commits

--- api-service/app/__init__.py
+++ api-service/app/__init__.py
@@ -9,7 +9,6 @@ def create_app():
 
     app.config.from_object(DevelopmentConfig)
 
-    # Register routes
     from app.routes.health_routes import health_bp
 
     app.register_blueprint(health_bp)
